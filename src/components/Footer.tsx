2f8db623f9d876c3af96755292ed9bdd8ae19d81
RobertoCastelli <roberto.castelli.teal@gmail.com>
2021-03-21 18:31:23 +0100

    second test branch

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Footer = () => {
+	return (
+		<footer>
+			<p>copyright &copy; 2021 Roberto Castelli</p>
+			<p>robertocastelli.dev</p>
+		</footer>
+	)
+}
+
+export default Footer
